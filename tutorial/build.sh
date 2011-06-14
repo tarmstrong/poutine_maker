71ec00fccd91bb9fe6805ae1f47c7a5f1170cc04
Tavish Armstrong <tavish@evolvingweb.ca>
2011-06-14 15:38:34 -0400

    fixed build.sh to deal with escaped characters

--- tutorial/build.sh
+++ tutorial/build.sh
@@ -2,3 +2,8 @@
 
 cat head.html > part1.html
 markdown part1.md >> part1.html
+sed -i 's|=&gt;|=>|g' part1.html
+sed -i 's|&amp;|\&|g' part1.html
+cat part1.html | perl -pe 's|(<h.>.*?)<code>(.*?)</code>|\1\2|' > part1.html.bk1
+cat part1.html.bk1 | perl -pe 's|<code>(.*?)</code>|<span style='\''font-family:"Courier New","DejaVu Sans Mono", monospace'\''>\1</span>|g' > part1.html.bk2
+cat part1.html.bk2 | sed 's|code>|code>\n|g' > part1.html
